28e8f20a3abfa8d2c128d9d37e5dedfd9c5fa6c8
toseina <82885855+toseina@users.noreply.github.com>
2024-11-20 23:30:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ async def find(image_path, confidence: float = 0.8):
     last_location = None
 
     while running:
-        location = pyautogui.locateOnScreen(image_path, confidence=confidence)
+        try:
+            location = pyautogui.locateOnScreen(image_path, confidence=confidence)
+            if location:
+                center = pyautogui.center(location)
 
-        if location:
-            center = pyautogui.center(location)
+                if last_location != center:
+                    last_location = center
+                    win32api.SetCursorPos((center.x, center.y))
+                    await asyncio.sleep(0.3) # you can change this
+                    pyautogui.click()
 
-            if last_location != center:
-                last_location = center
-                win32api.SetCursorPos((center.x, center.y))
-                pyautogui.click()
-                await asyncio.sleep(0.08)
+        except Exception:
+            await asyncio.sleep(0.1)
+            return
 
-        await asyncio.sleep(0.05)
-
-async def main(image_path, confidence):
+async def main(image_path, confidence: float):
     while True:
         if running:
             await find(image_path, confidence)
